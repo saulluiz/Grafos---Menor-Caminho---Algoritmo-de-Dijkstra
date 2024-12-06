929027d4cc144a0396b4e7b015212f2621cb6c14
Saulo Luiz <156348193+saulluiz@users.noreply.github.com>
2024-12-06 18:09:21 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,20 +4,19 @@ public class Program()
 {
     public static void Main()
     {
-        Dijkstra d = new Dijkstra(6,1);
-      
-        d.CreateEdge(1, 3, 12);
-        d.CreateEdge(1, 5, 2);
-        d.CreateEdge(1, 4, 1);
-        d.CreateEdge(5,2,9);
-        d.CreateEdge(5, 4, 3);
-        d.CreateEdge(4,3,5);
-        d.CreateEdge(3,2,8);
+ Dijkstra d = new Dijkstra(6, 0);
 
-       
-        d.ListAllRoutes();
+ d.CreateEdge(0, 2, 20);
+ d.CreateEdge(0, 1, 60);
+ d.CreateEdge(0, 3, 180);
+ d.CreateEdge(1, 3, 35);
+ d.CreateEdge(1, 4, 50);
+ d.CreateEdge(2, 5, 350);
+ d.CreateEdge(3, 2, 45);
+ d.CreateEdge(3, 5, 70);
+ d.CreateEdge(3, 4, 10);
 
-
+ d.ListAllRoutes();
 
     }
 }
